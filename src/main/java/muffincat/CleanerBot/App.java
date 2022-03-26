32457256e236cd61a94ee131246a4539d89c2ac4
Muffin Cat <matanshemaya@gmail.com>
2022-03-26 18:53:29 +0300

    Update App.java

--- src/main/java/muffincat/CleanerBot/App.java
+++ src/main/java/muffincat/CleanerBot/App.java
@@ -11,14 +11,14 @@ import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 
 public class App{
-    public static String prefex = "~";
+	public static String prefex = "~";
 	
-    public static void main(String[] args) throws LoginException{
+	public static void main(String[] args) throws LoginException{
         JDABuilder builder = JDABuilder.createDefault(System.getenv("TOKEN"));
         builder.setActivity(Activity.playing("Cleaning Simulator"));
         builder.setStatus(OnlineStatus.IDLE);
         
-        builder.addEventListeners(new Info(), new Clear());
+        builder.addEventListeners(new Info(), new Stopbot(), new Clear());
         builder.build();
-    }
+	}
 }
