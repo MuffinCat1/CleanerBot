c46f6b6b3abf23b454b0d3a7f131b7da9528acbf
Muffin Cat <matanshemaya@gmail.com>
2023-01-06 19:02:54 +0200

    Update Stopbot.java

--- src/main/java/muffincat/CleanerBot/Commands/Stopbot.java
+++ src/main/java/muffincat/CleanerBot/Commands/Stopbot.java
@@ -3,25 +3,27 @@ package muffincat.CleanerBot.Commands;
 import muffincat.CleanerBot.App;
 
 import net.dv8tion.jda.api.Permission;
-import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.entities.Member;
 
-public class Stopbot extends ListenerAdapter{
+public class Stopbot extends ListenerAdapter {
 	@Override
-	public void onGuildMessageReceived(GuildMessageReceivedEvent event)  {
-		String[] args = event.getMessage().getContentRaw().split("\\s+");
+	public void onMessageReceived(MessageReceivedEvent _event) {
+		if (!_event.isFromGuild()) return;
 		
-		if(args[0].equalsIgnoreCase(App.prefex + "stopbot")) {
-			Member member = event.getMember();
-			if(member != null) {
-				if(member.getPermissions().contains(Permission.ADMINISTRATOR)) {
-					event.getChannel().sendTyping().queue();
-					event.getChannel().sendMessage("Im going to sleep").queue();
+		String[] _args = _event.getMessage().getContentRaw().split("\\s+");
+		
+		if(_args[0].equalsIgnoreCase(App.PREFEX + "stopbot")) {
+			Member _member = _event.getMember();
+			if(_member != null) {
+				if(_member.getPermissions().contains(Permission.ADMINISTRATOR)) {
+					_event.getChannel().sendTyping().queue();
+					_event.getChannel().sendMessage("Im going to sleep").queue();
 					System.exit(0);
-				} if(!member.getPermissions().contains(Permission.ADMINISTRATOR)) {
-					event.getChannel().sendTyping().queue();
-					event.getChannel().sendMessage("Only Admins can stop me now!").queue();
+				} else {
+					_event.getChannel().sendTyping().queue();
+					_event.getChannel().sendMessage("Only Admins can stop me now!").queue();
 				}
 			}
 		}
