9293f9233ca60415bd6244b751a4d44f224739a1
Muffin Cat <matanshemaya@gmail.com>
2023-01-10 22:19:54 +0200

    Update App.java

--- src/main/java/muffincat/CleanerBot/App.java
+++ src/main/java/muffincat/CleanerBot/App.java
@@ -5,9 +5,11 @@ import java.util.EnumSet;
 import javax.security.auth.login.LoginException;
 
 import muffincat.CleanerBot.Commands.Clear;
+import muffincat.CleanerBot.Commands.CommandManager;
 import muffincat.CleanerBot.Commands.Info;
+import muffincat.CleanerBot.Commands.Stopbot;
+
 import net.dv8tion.jda.api.JDA;
-//import muffincat.CleanerBot.Commands.Stopbot; stopbot for tests only; if u want 
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
@@ -15,18 +17,17 @@ import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class App{
-	public static final String PREFEX = "~";
 	public static User ME;
 	
 	public static void main(String[] args) throws LoginException{
-        JDABuilder _builder = JDABuilder.createDefault("xxxxxxxxxxxxxxxxxxxx");
+        JDABuilder _builder = JDABuilder.createDefault("*************************");
         _builder.setActivity(Activity.playing("Cleaning Simulator"));
         
         _builder.setStatus(OnlineStatus.ONLINE);
         _builder.enableIntents(EnumSet.allOf(GatewayIntent.class));
-        _builder.addEventListeners(new Info(), new Clear()); 
+        _builder.addEventListeners(new CommandManager(), new Info(), new Clear(), new Stopbot()); 
         
         JDA _jda = _builder.build();
-        ME = _jda.retrieveUserById(your_id).complete(); //just put your discord id, and dont forget that your id is a long, not int
+        ME = _jda.retrieveUserById(user_id).complete();
 	}
 }
