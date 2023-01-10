22146d2d4342552441473c6234daf3913c5503fe
Muffin Cat <matanshemaya@gmail.com>
2023-01-10 22:45:40 +0200

    Update Clear.java

--- src/main/java/muffincat/CleanerBot/Commands/Clear.java
+++ src/main/java/muffincat/CleanerBot/Commands/Clear.java
@@ -39,7 +39,6 @@ public class Clear extends ListenerAdapter{
 						_usage.setTitle("Specify amount to delete");
 						_usage.setDescription("Usage: `/clear [# of messages]`");
 					
-						_event.getChannel().sendTyping().queue();
 						_event.getHook().sendMessageEmbeds(_usage.build()).queue();
 					
 					} else { 
@@ -58,14 +57,13 @@ public class Clear extends ListenerAdapter{
 								} else {
 									_event.getGuildChannel().deleteMessages(_messages).queue();
 								}
-							}
-															
-							EmbedBuilder _succes = new EmbedBuilder();
-							_succes.setColor(0x22ff2a);
-							_succes.setTitle("✅ Succesfully deleted " + amount + " messages");
 								
-							_event.getChannel().sendTyping().queue();
-							_event.getHook().sendMessageEmbeds(_succes.build()).queue();
+								EmbedBuilder _succes = new EmbedBuilder();
+								_succes.setColor(0x22ff2a);
+								_succes.setTitle("✅ Succesfully deleted " + amount + " messages");
+								
+								_event.getHook().sendMessageEmbeds(_succes.build()).queue();
+							}
 							
 						} catch (Exception _ex) {
 							_ex.printStackTrace();
@@ -76,7 +74,6 @@ public class Clear extends ListenerAdapter{
 								_error.setTitle("🔴 Too many messages selected");
 								_error.setDescription("only messages Between 1-100 can be deleted at one time (cannot delete 0 messages)");
 						
-								_event.getChannel().sendTyping().queue();
 								_event.getHook().sendMessageEmbeds(_error.build()).queue();
 					
 							} else if(_ex.toString().startsWith("java.lang.NumberFormatException: For input string")){
@@ -84,22 +81,19 @@ public class Clear extends ListenerAdapter{
 								_error.setTitle("🔴 Wrong Input");
 								_error.setDescription("Please write a number between 1-100");
 						
-								_event.getChannel().sendTyping().queue();
 								_event.getHook().sendMessageEmbeds(_error.build()).queue();								
 							
 							} else if(_ex.toString().startsWith("java.lang.IllegalArgumentException: Must provide at least 2")){
 								_error.setColor(0xff3923);
 								_error.setTitle("🔴 Something went wrong");
-								_error.setDescription("Please try again");
+								_error.setDescription("Too many messages were selected");
 						
-								_event.getChannel().sendTyping().queue();
 								_event.getHook().sendMessageEmbeds(_error.build()).queue();	
 							} else {
 								_error.setColor(0xff3923);
 								_error.setTitle("🔴 Selected messages are older then two weeks");
 								_error.setDescription("Messages older then two weeks cannot be deleted");
 						
-								_event.getChannel().sendTyping().queue();
 								_event.getHook().sendMessageEmbeds(_error.build()).queue();
 							}
 				
@@ -107,7 +101,6 @@ public class Clear extends ListenerAdapter{
 					}
 					
 				} else {
-					_event.getChannel().sendTyping().queue();
 					_event.getHook().sendMessage("Only Admins can use that command").queue();
 				}
 			 }
