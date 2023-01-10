686e21dd96744b0eccaa5f7638883ea23a81b5a3
Muffin Cat <matanshemaya@gmail.com>
2023-01-10 22:17:52 +0200

    Update Info.java

--- src/main/java/muffincat/CleanerBot/Commands/Info.java
+++ src/main/java/muffincat/CleanerBot/Commands/Info.java
@@ -3,29 +3,32 @@ package muffincat.CleanerBot.Commands;
 import muffincat.CleanerBot.App;
 
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+//import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-public class Info extends ListenerAdapter{
-	@Override
-	public void onMessageReceived(MessageReceivedEvent _event) {
-		if (!_event.isFromGuild()) return;
-		String[] _args = _event.getMessage().getContentRaw().split("\\s+"); 
-		
-		if(_args[0].equalsIgnoreCase(App.PREFEX + "info")) {			
-			_event.getChannel().sendTyping().queue();
+import org.jetbrains.annotations.NotNull;
 
+public class Info extends ListenerAdapter{
+
+	@Override
+	public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent _event) {
+		if (!_event.isFromGuild()) return;
+		
+		if(_event.getName().equals("info")) {			
+			_event.getChannel().sendTyping().queue();
+			
 			EmbedBuilder _info = new EmbedBuilder();
 			_info.setTitle("🧹Information");
-			_info.setDescription("A bot that can clean up to 100 messages that are two weeks old, he cannot delete messages that are higher than two weeks old. You can use the \"~\" + "
-					+ "commad for example `~clean 100` will clean 100 messages");
-			_info.addField("🛠️Commands","`~info` - show info on the bot\n`~clear [# of messages]` - clear a number of messages that you put up to a 100", false);
-			//_info.addField("⚙️Options","`noEmbed` - disables the embed message after the bot finishes deleting the messages", false); //maybe in future 
+			_info.setDescription("A bot that can clean up to 100 messages that are two weeks old, he can't delete messages that are higher than two weeks old. You can use the `~` + "
+					+ "`clear` to clean the messages; for example `~clear 100` will clean 100 messages");
+			_info.addField("🛠️Commands","`/info` - show info on the bot\n`~clear [# of messages]` - clear a number of messages that you enter up to a 100", false);
+			//_info.addField("⚙️Options","`noEmbed` - disables the embed message after the bot finishes deleting the messages", false); //maybe in the future 
 			_info.setColor(0xF23309);
 			
 			_info.setFooter(String.format("Created By %s",App.ME.getAsTag()), App.ME.getAvatarUrl());
-			 
-			_event.getChannel().sendMessageEmbeds(_info.build()).queue();
+			
+			_event.replyEmbeds(_info.build()).queue();
 			_info.clear();
 		}
 	}
